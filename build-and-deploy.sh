1267fe6832a0807dd3718c5363ed4d772f524189
Hussein Habibi Juybari <hussein.juybari@gmail.com>
2022-01-12 02:13:09 +0330

    Update build-and-deploy.sh

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -8,7 +8,7 @@ GITHUB_ACTOR=$3 &&
 echo '===> Start running scripts for jekyll build.' &&
 
 cd ./dist &&
-remote_repo="https://x-access-token:${ACCESS_TOKEN}@github.com/chabok-io/chabok-io.github.io.git" &&
+remote_repo="https://${{ACCESS_TOKEN}}:x-oauth-basic@github.com/chabok-io/chabok-io.github.io.git" &&
 remote_branch="gh-pages" && 
 
 echo remote_branch $remote_branch &&
